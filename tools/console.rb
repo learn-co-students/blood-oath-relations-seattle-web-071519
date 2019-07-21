01a82f9985e2503d4f97fa4e4e4e77a4a765f3fe
marshallhelenm <marshallhelenm@gmail.com>
2019-07-21 12:50:08 -0700

    done except the bonus stuff

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#some cults to work with
+the_abyss = Cult.new("The Abyss", "Seattle", 1992, "We have cookies.")
+hums = Cult.new("He Who Hums", "Seattle", 1980, "Hum your own tune.")
+yarn = Cult.new("Fibercraft Club", "Portland", 2012, "You can never have enough yarn!")
+
+#some followers to work with
+neil = Follower.new("Neil Gaiman", 51, "It's better in the dark.")
+terry = Follower.new("Terry Pratchett", 72, "It's not worth doing something unless someone, somewhere, would much rather you weren't doing it.")
+dresden = Follower.new("Harry Dresden", 39, "Fight back.")
+
+#some bloodoaths to work with
+neil.join_cult(the_abyss)
+terry.join_cult(the_abyss)
+dresden.join_cult(the_abyss)
+neil.join_cult(hums)
+terry.join_cult(hums)
+neil_yarn = BloodOath.new(yarn, neil)
+
+
 binding.pry
 
 puts "Mwahahaha!" # just in case pry is buggy and exits
